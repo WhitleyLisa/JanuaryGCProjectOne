50f14b5f917190a064f15d62fd036e3fba2be10e
SoniaRoss <srross99@gmail.com>
2022-03-23 22:32:27 -0400

    Add properties to DAL Member.cs

--- LibraryApp/LibraryApp/DataAccessLayer/Models/Member.cs
+++ LibraryApp/LibraryApp/DataAccessLayer/Models/Member.cs
@@ -8,5 +8,11 @@ namespace LibraryApp
 {
     public class Member
     {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }    
+        public string email { get; set; }
+        public bool isActive { get; set; }
+        public List<Book> books { get; set; }
     }
 }
