28753f1f353cf7a81ff6560b85453a363186176c
Jay <biskyniffer@gmail.com>
2022-03-24 20:22:19 -0400

    where is it

---
+++ LibraryApp/LibraryApp/DataAccessLayer/Repositories/BookRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.DataAccessLayer.Repositories
+{
+    public class BookRepo
+    {
+        private List<Book> BookList { get; set; } = new List<Book> { Book };
+
+        public Book DisplayBooks()
+        {
+            string executableLocation = AppDomain.CurrentDomain.BaseDirectory;
+
+            string[] files = Directory.GetFiles(executableLocation + @"\Database\BookTable\", "*.json", SearchOption.TopDirectoryOnly);
+        }
+    }
+}
