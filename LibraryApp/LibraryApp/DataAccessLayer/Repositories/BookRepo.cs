174a8c73e547f9aca9ecc4257f2f8fa89c537d54
Lisa Van Huijkelom <lisa.vanhuijkelom@infinicept.com>
2022-03-27 08:17:03 -0600

    Updating the JSON

--- LibraryApp/LibraryApp/DataAccessLayer/Repositories/BookRepo.cs
+++ LibraryApp/LibraryApp/DataAccessLayer/Repositories/BookRepo.cs
@@ -33,7 +33,7 @@ namespace LibraryApp.DataAccessLayer.Repositories
                 }
             }
 
-            books = books.OrderBy(x => x.isbn).ToList();
+            books = books.OrderBy(x => x.id).ToList();
 
             return books;
 
@@ -55,6 +55,14 @@ namespace LibraryApp.DataAccessLayer.Repositories
             return book.Where(x => x.title.ToLower().Contains(keyword.ToLower())).ToList();
         }
 
+        public void UpdateBook(Book book)
+        {
+            /// get book from the list 
+            
+
+            /// 
+        }
+
         //public string DisplayBooks(List<Book>)
         //{
         //    foreach
@@ -64,7 +72,7 @@ namespace LibraryApp.DataAccessLayer.Repositories
         //private void AddToCache(Book book)
         //{
         //    //Any is a boolean. It checks if if one or more objects fit this criteria
-        //    if (!Cache.Any(x => x.isbn == book.isbn))
+        //    if (!Cache.Any(x => x.id == book.id))
         //    {
         //        Cache.Add(book);
         //    }
