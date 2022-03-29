d6b7d4fa98f3ef2fdd8c2d5bc889b570368ffc65
Jay <biskyniffer@gmail.com>
2022-03-28 21:44:34 -0400

    mvp done

--- LibraryApp/LibraryApp/Program.cs
+++ LibraryApp/LibraryApp/Program.cs
@@ -11,7 +11,7 @@ var librarian = new Librarian();
 while (repeat)
 {
     Console.WriteLine("\nWelcome to the library! What would you like to do? (Please choose a number below)" +
-        "\n 1. View list of books/Check out a book 2. Search books 3. Return a book 4. Exit");
+        "\n 1. View list of books 2. Search books 3. Check out a book 4. Return a book 5. Exit");
     int userInput;
     bool tryParseBool = int.TryParse(Console.ReadLine(), out userInput);
 
@@ -22,22 +22,6 @@ while (repeat)
             case 1:
                 var displayMe = librarian.getBookList();
                 librarian.DisplayList(displayMe);
-                Console.WriteLine("\nWould you like to check out one of these books?(Y/N)");
-                var checkOutInput = Console.ReadLine().ToLower();
-                if (checkOutInput == "y")
-                {
-                    //Ensures user input is a valid option
-                    Console.WriteLine("\nPlease enter the number of the book you would like to check out:");
-                    int checkOutInt;
-                    bool checkOutInputParse  = int.TryParse(Console.ReadLine(), out checkOutInt);
-                    while (!checkOutInputParse || checkOutInt >= displayMe.Count || checkOutInt <= 0)
-                    {
-                        Console.WriteLine(error);
-                        checkOutInputParse = int.TryParse(Console.ReadLine(), out checkOutInt);
-                    }
-                    //Adjusts the users input to match the index of the book they've selected                    
-                    librarian.CheckOutBook(displayMe[checkOutInt-1]);
-                }
                 break;
 
             case 2:
@@ -46,10 +30,35 @@ while (repeat)
                 break;
 
             case 3:
-                Console.WriteLine("\nReturn book stuff");
+                var checkOutDisplayMe = librarian.getBookList();
+                librarian.DisplayList(checkOutDisplayMe);
+                Console.WriteLine("\nPlease enter the number of the book you would like to check out:");
+                int checkOutInt;
+                bool checkOutInputParse = int.TryParse(Console.ReadLine(), out checkOutInt);
+                while (!checkOutInputParse || checkOutInt >= checkOutDisplayMe.Count || checkOutInt <= 0)
+                {
+                    Console.WriteLine(error);
+                    checkOutInputParse = int.TryParse(Console.ReadLine(), out checkOutInt);
+                }
+                //Adjusts the users input to match the index of the book they've selected                    
+                librarian.CheckOutBook(checkOutDisplayMe[checkOutInt - 1]);
                 break;
 
             case 4:
+                var returnDisplayMe = librarian.getBookList();
+                librarian.DisplayList(returnDisplayMe);
+                Console.WriteLine("\nPlese enter the number of the book you would like to return:");
+                int returnInt;
+                bool returnParse = int.TryParse(Console.ReadLine(), out returnInt);
+                while(!returnParse || returnInt >= returnDisplayMe.Count || returnInt <= 0)
+                {
+                    Console.WriteLine(error);
+                    returnParse = int.TryParse(Console.ReadLine(), out returnInt);
+                }
+                librarian.ReturnBook(returnDisplayMe[returnInt - 1]);
+                break;
+
+            case 5:
                 repeat = false;
                 break;
 
